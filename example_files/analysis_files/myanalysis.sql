a097a15b11413f6ba2c426a47f9a6ba706e01e60
Julio <julio@smartnow.la>
2021-11-02 11:38:24 -0600

    se incluye ejemplo de analisis

---
+++ example_files/analysis_files/myanalysis.sql
@@ -0,0 +1,9 @@
+select
+    p."id" as id,
+    p."name" as name,
+    date_format('%Y-%m-%d',p."birth") as birth,
+    cc."country_name" as country,
+    current_timestamp(0) as updated_at
+from {{ ref('person') }} p
+left join {{ ref('country_codes') }} cc
+on cc.country_code=p.country_code
